51290a5555cafcc3135f7e8f4fd39ce05e917d7c
Koushik Sarkar <150505459+kidKrix@users.noreply.github.com>
2024-07-06 01:12:17 +0530

    Update Rock-Paper-Scissor.c

--- Rock-paper-scissor.c
+++ Rock-paper-scissor.c
@@ -31,51 +31,60 @@ void logic()
     case 1: // user=rock, comp=rock
         if (computer == 1)
         {
-            printf("\n You = Rock \n Computer = Rock");
+            printf("\n You choose Rock \n Computer choose Rock");
+            printf("\n Selected items are the same");
             printf("\n Game Draw");
         }
         else if (computer == 2) // user=rock, comp=paper
         {
-            printf("\n You = Rock \n Computer = Paper");
+            printf("\n You choose Rock \n Computer choose Paper");
+            printf("\n Paper covers Rock");
             printf("\n Computer Won");
         }
         else // user=rock, comp=scissor
         {
-            printf("\n You = Rock \n Computer = Scissor");
+            printf("\n You choose Rock \n Computer choose Scissor");
+            printf("\n Rock crushes Scissors");
             printf("\n You Won");
         }
         break;
     case 2: // user=paper
         if (computer == 1)
         {
-            printf("\n You = Paper \n Computer = Rock");
+            printf("\n You choose Paper \n Computer choose Rock");
+            printf("\n Paper covers Rock");
             printf("\n You Won");
         }
         else if (computer == 2) // user=paper, comp=paper
         {
-            printf("\n You = Paper \n Computer = Paper");
+            printf("\n You choose Paper \n Computer choose Paper");
+            printf("\n Selected items are the same");
             printf("\n Game Draw");
         }
         else // user=paper, comp=scissor
         {
-            printf("\n You = Paper \n Computer = Scissor");
+            printf("\n You choose Paper \n Computer choose Scissor");
+            printf("\n Scissors cut Paper");
             printf("\n Computer Won");
         }
         break;
     case 3: // user=scissor
         if (computer == 1)
         {
-            printf("\n You = Scissor \n Computer = Rock");
+            printf("\n You choose Scissor \n Computer choose Rock");
+            printf("\n Rock crushes Scissors");
             printf("\n Computer Won");
         }
         else if (computer == 2) // user=scissor, comp=paper
         {
-            printf("\n You = Scissor \n Computer = Paper");
+            printf("\n You choose Scissor \n Computer choose Paper");
+            printf("\n Scissors cut Paper");
             printf("\n You Won");
         }
         else // user=scissor, comp=scissor
         {
-            printf("\n You = Scissor \n Computer = Scissor");
+            printf("\n You choose Scissor \n Computer choose Scissor");
+            printf("\n Selected items are the same");
             printf("\n Game Draw");
         }
         break;
